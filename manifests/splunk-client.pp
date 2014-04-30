d1c3a6b9b2dbf910f9a09557a64fac1cce21e76c
Max Griffiths <maxgriff@thoughtworks.com>
2014-04-29 22:43:14 -0700

    Add files for testing transforms

--- manifests/splunk-client.pp
+++ manifests/splunk-client.pp
@@ -1,10 +1,24 @@
 $splunk_ver = '6.0.3-204106-linux-2.6-amd64'
-$bin_path = '/opt/splunkforwarder/bin'
+$splunk_root = '/opt/splunkforwarder'
 
 package { 'splunk':
   source   => "/vagrant/sw/splunkforwarder-${splunk_ver}.deb",
   provider => dpkg,
   notify => Service['splunk'],
+} ->
+
+file { "${splunk_root}/etc/system/local/inputs.conf":
+  source => "/vagrant/files/client/inputs.conf",
+  owner => root,
+  group => root,
+  mode  => 0600,
+} ->
+
+file { "${splunk_root}/etc/system/local/outputs.conf":
+  source => "/vagrant/files/client/outputs.conf",
+  owner => root,
+  group => root,
+  mode  => 0600,
 } ->
 
 file { '/etc/init/splunk.conf':
--- manifests/splunk-server.pp
+++ manifests/splunk-server.pp
@@ -1,14 +1,35 @@
 $splunk_ver = '6.0.3-204106-linux-2.6-amd64'
-$bin_path = '/opt/splunkforwarder/bin'
+$splunk_root = '/opt/splunk'
 
 package { 'splunk':
-  content   => template("/vagrant/sw/splunk-${splunk_ver}.deb"),
+  source => "/vagrant/sw/splunk-${splunk_ver}.deb",
   provider => dpkg,
   notify => Service['splunk'],
 } ->
 
+file { "${splunk_root}/etc/system/local/inputs.conf":
+  source => "/vagrant/files/server/inputs.conf",
+  owner => root,
+  group => root,
+  mode  => 0600,
+} ->
+
+file { "${splunk_root}/etc/system/local/props.conf":
+  source => "/vagrant/files/server/props.conf",
+  owner => root,
+  group => root,
+  mode  => 0600,
+} ->
+
+file { "${splunk_root}/etc/system/local/transforms.conf":
+  source => "/vagrant/files/server/transforms.conf",
+  owner => root,
+  group => root,
+  mode  => 0600,
+} ->
+
 file { '/etc/init/splunk.conf':
-  source => "/vagrant/files/splunk.init",
+  content => template('/vagrant/templates/splunk.init'),
   owner => root,
   group => root,
   mode  => 0600,
